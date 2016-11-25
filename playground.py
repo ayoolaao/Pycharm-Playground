6acfe0a73273c29e53b7c61a270559b1b9a58854
Ayoolaao <ao.abimbola@gmail.com>
2016-11-25 00:36:24 -0600

    code to draw a circle from a series of squares

--- playground.py
+++ playground.py
@@ -1,39 +1,12 @@
 import turtle
 
 
-def draw_circle():
+def draw_square(my_square):
 
-    my_circle = turtle.Turtle()
-    my_circle.shape("circle")
-    my_circle.circle(75)
-    my_circle.color("#46FF18")
-
-
-def draw_square():
-
-    my_square = turtle.Turtle()
-    my_square.shape("arrow")
-    my_square.color("red")
-    my_square.speed(2)
-
-    turn = [1, 2, 3, 4]
-
-    for i in turn:
+    for i in range(1,5):
+        my_square.speed(0.5)
         my_square.forward(100)
         my_square.right(90)
-
-
-def draw_triangle():
-    my_triangle = turtle.Turtle()
-    my_triangle.shape("arrow")
-    my_triangle.color("blue")
-    my_triangle.speed(5)
-
-    turn = [1, 2, 3]
-
-    for i in turn:
-        my_triangle.right(120)
-        my_triangle.forward(200)
 
 
 def draw_shapes():
@@ -41,9 +14,10 @@ def draw_shapes():
     window = turtle.Screen()
     window.bgcolor("white")
 
-    draw_square()
-    draw_circle()
-    draw_triangle()
+    ayo = turtle.Turtle()
+    for i in range(0,360):
+        draw_square(ayo)
+        ayo.right(10)
 
     window.exitonclick()
 
