21da8480804def81b08432fffec18ac52ec86c1f
Ayoolaao <ao.abimbola@gmail.com>
2016-11-24 22:07:47 -0600

    draw a square and circle using functions only

--- playground.py
+++ playground.py
@@ -1,22 +1,37 @@
 import turtle
 
 
+def draw_circle():
+
+    my_circle = turtle.Turtle()
+    my_circle.shape("circle")
+    my_circle.circle(75)
+    my_circle.color("white")
+
+
 def draw_square():
 
-    window = turtle.Screen()
-    window.bgcolor("#80CDFF")
-
-    ayo = turtle.Turtle()
-    ayo.shape("turtle")
-    ayo.color("#000000")
-    ayo.speed(2)
+    my_square = turtle.Turtle()
+    my_square.shape("turtle")
+    my_square.color("#000000")
+    my_square.speed(2)
 
     turn = [1 ,2, 3, 4]
 
     for i in turn:
-        ayo.forward(100)
-        ayo.right(90)
+        my_square.forward(100)
+        my_square.right(90)
+
+
+def draw_shapes():
+
+    window = turtle.Screen()
+    window.bgcolor("#80CDFF")
+
+    draw_square()
+    draw_circle()
 
     window.exitonclick()
 
-draw_square()
+
+draw_shapes()
