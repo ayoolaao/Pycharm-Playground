08e0290c0be856de5c1c334f6bb9d1a87960afb5
Ayoolaao <ao.abimbola@gmail.com>
2016-11-26 16:51:21 -0600

    added show info methods

--- playground_inheritance.py
+++ playground_inheritance.py
@@ -4,6 +4,10 @@ class Parent():
         self.last_name = last_name
         self.eye_color = eye_color
 
+    def show_info(self):
+        print ("Last Name: " + self.last_name)
+        print ("Eye Color: " + self.eye_color)
+
 
 class Child(Parent):
     def __init__(self, last_name, eye_color, number_of_toys):
@@ -11,9 +15,13 @@ class Child(Parent):
         Parent.__init__(self, last_name, eye_color)
         self.number_of_toys = number_of_toys
 
-kola = Parent("Abimbola", "Brown")
-print(kola.eye_color)
+    def show_info(self):
+        print ("Last Name: " + self.last_name)
+        print ("Eye Color: " + self.eye_color)
+        print ("Number of Toys: " + str(self.number_of_toys))
 
-ayo = Child("Kidd", "Blue", 7)
-print(ayo.last_name)
-print(ayo.number_of_toys)
+kola = Parent("Grown", "Blue")
+kola.show_info()
+
+jin = Child("Young", "Gold", 9)
+jin.show_info()
