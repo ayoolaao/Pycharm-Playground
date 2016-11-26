1c0b09a27bff8e300388fea16603b6f36385c8b5
Ayoolaao <ao.abimbola@gmail.com>
2016-11-26 02:55:15 -0600

    check for profanity using urllib

--- playground2_profanity_editor.py
+++ playground2_profanity_editor.py
@@ -1,8 +1,18 @@
+import urllib
+
 
 def read_text():
     quotes = open("/Users/ayoolaao/Downloads/movie_quotes.txt")
     contents = quotes.read()
     print (contents)
     quotes.close()
+    check_profanity(contents)
+
+
+def check_profanity(text_to_check):
+    connection = urllib.urlopen("http://www.wdylike.appspot.com/?q=" + text_to_check)
+    output = connection.read()
+    print (output)
+    connection.close()
 
 read_text()
